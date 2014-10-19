e204e5bb0dcd9e525cd9c9350f538fa14625c345
heshaowei920 <heshaowei920>
2014-10-19 16:53:56 +0800

    as

---
+++ src/com/jyoffer/model/WorkerVO.java
@@ -0,0 +1,23 @@
+package com.jyoffer.model;
+
+public class WorkerVO {
+	private String userID;
+	private String name;
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
